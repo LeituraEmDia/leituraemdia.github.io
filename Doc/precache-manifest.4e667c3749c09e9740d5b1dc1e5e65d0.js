fdbf4625959a6d60a475f99c57f9c94332b3fbde
CaioCesarLima <cesarlimacaio@gmail.com>
2018-08-14 22:49:23 -0300

    first commit

---
+++ Doc/precache-manifest.4e667c3749c09e9740d5b1dc1e5e65d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "f848f61f25c2065377e8",
+    "url": "/js/chunk-vendors.f848f61f.js"
+  },
+  {
+    "revision": "7f860e99cb6e66e70632",
+    "url": "/js/app.7f860e99.js"
+  },
+  {
+    "revision": "c38e88a4e5a0fb726db2167e36692b0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f848f61f25c2065377e8",
+    "url": "/css/chunk-vendors.3c36a9c9.css"
+  },
+  {
+    "revision": "7f860e99cb6e66e70632",
+    "url": "/css/app.4b52c3cb.css"
+  }
+];
